b2d4524ea2d5a7232453fa3879e644299ae6eee7
shubham agarwal <shubham.agarwal@johnlewis.co.uk>
2025-09-17 09:33:44 +0530

    Add python code to train a vanilla neural network

---
+++ Neural_Network/ReadMe.md
@@ -0,0 +1,17 @@
+# create a virtual env
+python3 -m venv venv
+
+# activate virtual env
+source venv/bin/activate
+
+# install library
+pip3 install numpy
+
+# run script
+python3 TrainNeuralNetwork.py
+
+# Deactivate virtual env
+deactivate
+
+# Blog
+https://victorzhou.com/blog/intro-to-neural-networks/
---
+++ Neural_Network/helpers.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def mse_loss(y_true, y_pred):
+  # y_true and y_pred are numpy arrays of the same length.
+  return ((y_true - y_pred) ** 2).mean()
+
+def sigmoid(x):
+  # Sigmoid activation function: f(x) = 1 / (1 + e^(-x))
+  return 1 / (1 + np.exp(-x))
+
+def deriv_sigmoid(x):
+  # Derivative of sigmoid: f'(x) = f(x) * (1 - f(x))
+  fx = sigmoid(x)
+  return fx * (1 - fx)
+
+y_true = np.array([1, 0, 0, 1])
+y_pred = np.array([0, 0, 0, 0])
+
+print(mse_loss(y_true, y_pred)) # 0.5
